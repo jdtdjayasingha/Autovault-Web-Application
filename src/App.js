d15ca8a1f437879051e83076985ab9728187f523
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-15 20:02:34 +0530

    Added login Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import loginLogo from './assets/images/loginLogo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='login-title'>AUTOVAULT</h1>
+        <div>
+          <img className='login-logo' src={loginLogo} alt="Login Logo" />
+        </div>
         <p className='login-sub-title'>Welcome Back</p>
         <input className='login-input' placeholder='Email' />
         <input className='login-input' placeholder='Password' />
