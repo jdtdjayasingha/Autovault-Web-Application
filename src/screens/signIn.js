225b319ccb493feb18e713f5301231b609cf38cf
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-16 09:30:57 +0530

    Updated Sign In page

--- src/screens/signIn.js
+++ src/screens/signIn.js
@@ -1,1 +1,20 @@
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='login-title'>AUTOVAULT</h1>
+        <p className='login-sub-title'>Create account</p>
+        <input className='login-input' placeholder='User Name' />
+        <input className='login-input' placeholder='Password' />
+        <input className='login-input' placeholder='Password' />
+        <button className='login-button'>Sign UP</button>
+        <p className='up-in-account'>Already have an account</p>
+        <a href='#' className='up-in-link'>Sign In</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
