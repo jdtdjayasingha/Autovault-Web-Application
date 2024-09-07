297dd297c0d79f791787065cd2dfb1d3df6b3dd9
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-09-07 21:10:42 +0530

    Modified App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import User from './pages/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+	<Routes>
+	  <Route path='/' element={<App />} />
+    <Route path='/user' element={<User />} />
+	</Routes>
+  </BrowserRouter>
 );
